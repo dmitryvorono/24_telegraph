6c5ff1d1b51375564e6acc1874ce0ca70a5f2327
dvoronov <voronov-d88@yandex.ru>
2017-08-24 14:16:20 +0300

    Add save form to articles

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+import os
+import json
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def form():
+    if request.method== 'POST':
+        article = {'header': request.form['header'],
+                   'signature': request.form['signature'],
+                   'body': request.form['body']}
+        json_dump(article, ''.join(['articles/', article['header'], '.json']))
     return render_template('form.html')
+
+
+def json_dump(dumping_dict, filepath):
+    with open(filepath, 'w') as file_handler:
+        json.dump(dumping_dict, file_handler)
+
 
 if __name__ == "__main__":
     app.run()
