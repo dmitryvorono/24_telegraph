f05c79a3ce1190d3fd435b47ecaca74fd55310f5
dvoronov <voronov-d88@yandex.ru>
2017-08-24 15:28:05 +0300

    Set cookies on client

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
 import json
-from flask import Flask, render_template, request, redirect, url_for
+import random
+from flask import Flask, render_template, request, redirect, url_for, make_response
 
 app = Flask(__name__)
 
@@ -9,10 +10,14 @@ def form():
     if request.method== 'POST':
         article = {'header': request.form['header'],
                    'signature': request.form['signature'],
-                   'body': request.form['body']}
+                   'body': request.form['body'],
+                   'unid': generate_unid()}
         article_filename = ''.join(['articles/', article['header'], '.json'])
         json_dump(article, article_filename)
-        return redirect(url_for('render_article', article_name = article['header']))
+        redirect_to_article = redirect(url_for('render_article', article_name = article['header']))
+        response = make_response(redirect_to_article)
+        response.set_cookie(article['unid'], value='1')
+        return response
     return render_template('form.html')
 
 
@@ -34,6 +39,10 @@ def json_load(filepath):
         return None
     with open(filepath, 'r') as file_handler:
         return json.load(file_handler)
+
+
+def generate_unid():
+    return ''.join([str(hex(random.randrange(16)))[2] for x in range(32)])
 
 
 if __name__ == "__main__":
