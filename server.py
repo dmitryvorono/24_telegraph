cd597dcbf2a1c07baec4932411a5f8761c4416c5
dvoronov <voronov-d88@yandex.ru>
2017-09-21 07:47:43 +0300

    Refactoring

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 import os
 import json
-import random
 from flask import Flask, render_template, request, redirect, url_for, make_response, abort
 from datetime import date
 from transliterate import translit
 import re
 import uuid
+from http import HTTPStatus
 
 
 app = Flask(__name__,
@@ -16,15 +16,14 @@ app = Flask(__name__,
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
-    if request.method== 'POST':
-        current_date = date.today()
+    if request.method == 'POST':
         article = {'header': request.form['header'],
                    'signature': request.form['signature'],
                    'body': request.form['body'],
                    'unid': str(uuid.uuid4())}
         article_filepath, article['filename'] = generate_unique_article_name(article)
         json_dump(article, article_filepath)
-        redirect_to_article = redirect(url_for('render_article', article_name = article['filename']))
+        redirect_to_article = redirect(url_for('render_article', article_name=article['filename']))
         response = make_response(redirect_to_article)
         response.set_cookie(article['unid'], value='1')
         return response
@@ -36,7 +35,7 @@ def render_article(article_name):
     article_filename = 'articles/{0}.json'.format(article_name)
     article = json_load(article_filename)
     if not article:
-        abort(404)
+        abort(HTTPStatus.NOT_FOUND.value)
     can_edit = True if article['unid'] in request.cookies else False
     if can_edit and request.method == 'POST':
         new_article_values = {'header': request.form['header'],
@@ -44,13 +43,13 @@ def render_article(article_name):
                               'body': request.form['body']}
         article.update(new_article_values)
         json_dump(article, article_filename)
-        return redirect(url_for('render_article', article_name = article_name))
+        return redirect(url_for('render_article', article_name=article_name))
     return render_template('article.html', article=article, can_edit=can_edit)
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-        return render_template('404.html'), 404
+@app.errorhandler(HTTPStatus.NOT_FOUND.value)
+def page_not_found(error):
+    return render_template('404.html'), HTTPStatus.NOT_FOUND.value
 
 
 def json_dump(dumping_dict, filepath):
@@ -67,7 +66,7 @@ def json_load(filepath):
 
 def generate_unique_article_name(article, length_name=100):
     article_header_latin = translit(article['header'], "ru", reversed=True)
-    prepared_article_header_latin = re.sub(' ', '-', re.sub('\W+',' ', article_header_latin))[:length_name]
+    prepared_article_header_latin = re.sub(' ', '-', re.sub('\W+', ' ', article_header_latin))[:length_name]
     index_article = 1
     current_date = date.today()
     article_filename = '{0}-{1}-{2}-{3}'.format(prepared_article_header_latin, current_date.month, current_date.day, index_article)
@@ -76,7 +75,7 @@ def generate_unique_article_name(article, length_name=100):
         index_article += 1
         article_filename = '{0}-{1}-{2}-{3}'.format(prepared_article_header_latin, current_date.month, current_date.day, index_article)
         article_filepath = 'articles/{0}.json'.format(article_filename)
-    return (article_filepath, article_filename)
+    return article_filepath, article_filename
 
 
 if __name__ == "__main__":
